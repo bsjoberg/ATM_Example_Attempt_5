88113747026c2cd8320777e36cca0c44893d2e59
bsjoberg <brian.sjoberg@excella.com>
2019-09-23 11:36:00 -0400

    Stubbed out with null returns and asserEquals in then step.

--- src/main/java/fun/Hello_World.java
+++ src/main/java/fun/Hello_World.java
@@ -1,4 +1,7 @@
 package fun;
 
 public class Hello_World {
+    public String sayHi() {
+        return null;
+    }
 }
--- src/test/java/fun/Hello_World_Steps.java
+++ src/test/java/fun/Hello_World_Steps.java
@@ -4,22 +4,26 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import static org.junit.Assert.assertEquals;
+
 public class Hello_World_Steps {
+    private Hello_World hello_world;
+    private String message;
+
     @Given("I have a hello world app")
     public void i_have_a_hello_world_app() {
         // Write code here that turns the phrase above into concrete actions
-        Hello_World hello_world = new Hello_World();
+        hello_world = new Hello_World();
     }
 
     @When("I say hi")
     public void i_say_hi() {
         // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        message = hello_world.sayHi();
     }
 
     @Then("I hear {string}")
-    public void i_hear(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    public void i_hear(String expectedMessage) {
+        assertEquals(expectedMessage, message);
     }
 }
