d90ae41471977aec402788ce42f4466b7f14b013
bsjoberg <brian.sjoberg@excella.com>
2019-10-20 09:10:55 -0400

    Using Junit xml output format.

--- src/test/java/RunCukeTests.java
+++ src/test/java/RunCukeTests.java
@@ -3,6 +3,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = "pretty", features = "src/test/resources")
+@CucumberOptions(plugin = {"pretty", "junit:target/cucumber-reports/output.xml"},
+                features = "src/test/resources")
 public class RunCukeTests {
 }
