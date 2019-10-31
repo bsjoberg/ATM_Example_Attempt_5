48d4e140680a0188f680be1a8b3473fb6e4fa265
bsjoberg <brian.sjoberg@excella.com>
2019-10-31 01:29:05 -0400

    Fixed adding with cents over 200. Added more testing for the rest of Money. Cleaned up code and addressed warnings.

--- src/test/java/nicebank/MoneyTest.java
+++ src/test/java/nicebank/MoneyTest.java
@@ -3,6 +3,7 @@ package nicebank;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class MoneyTest {
 
@@ -15,7 +16,47 @@ public class MoneyTest {
 
     @Test
     public void addMoneyOver100Cents() {
-        Money money = new Money(100, 00);
+        Money money = new Money(100, 0);
         assertEquals(new Money("$101.50"), money.add(new Money(0, 150)));
+    }
+
+    @Test
+    public void addMoneyOver200Cents() {
+        Money money = new Money(20, 0);
+        assertEquals(new Money(22, 10), money.add(new Money(0, 210)));
+    }
+
+    @Test
+    public void convertToString() {
+        Money money = new Money(80, 10);
+        assertEquals("$80.10", money.toString());
+    }
+
+    @Test
+    public void twoMoneysEqual() {
+        Money firstMoney = new Money("$83.45");
+        Money secondMoney = new Money(83, 45);
+        assertEquals(firstMoney, secondMoney);
+    }
+
+    @Test
+    public void twoMoneysNotEqualCents() {
+        Money firstMoney = new Money("12.55");
+        Money secondMoney = new Money(12, 22);
+        assertNotEquals(firstMoney, secondMoney);
+    }
+
+    @Test
+    public void twoMoneysNotEqualsDollars() {
+        Money firstMoney = new Money("13.12");
+        Money secondMoney = new Money(12, 12);
+        assertNotEquals(firstMoney, secondMoney);
+    }
+
+    @Test
+    public void shouldNotBeEqualOneMoneyOneNot() {
+        Money firstMoney = new Money("$20.12");
+        String moneyString = "$20.12";
+        assertNotEquals(firstMoney, moneyString);
     }
 }
